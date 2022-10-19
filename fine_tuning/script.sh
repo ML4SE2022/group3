32ac380ca38cc67c12a0c6620eeeb4186ea0cda1
imarijnissen <i.c.w.m.marijnissen@student.tudelft.nl>
2022-10-19 19:25:20 +0200

    Make script dependent on input

--- fine_tuning/script.sh
+++ fine_tuning/script.sh
@@ -1,14 +1,24 @@
 # shellcheck disable=SC1066
 # shellcheck disable=SC2121
-pretrained_model="microsoft/codebert-base"
+
+if [ "$1" = "plbart" ]; then
+  pretrained_model="uclanlp/plbart-base"
+  model_type="plbart"
+  config_and_token_name="uclanlp/plbart-base"
+else
+  pretrained_model="microsoft/codebert-base"
+  model_type="roberta"
+  config_and_token_name="roberta-base"
+fi
+
 output_dir="./out/"
 python3 run.py \
 	--do_train \
 	--do_eval \
-	--model_type roberta \
+	--model_type $model_type \
 	--model_name_or_path $pretrained_model \
-	--config_name roberta-base \
-	--tokenizer_name roberta-base \
+	--config_name $config_and_token_name \
+	--tokenizer_name $config_and_token_name \
 	--train_filename ./train.java-python.java,./train.java-python.py \
 	--dev_filename ./valid.java-python.java,./valid.java-python.py \
 	--output_dir $output_dir \
