5fbf0705d4821df456fb3ba2e5c55a84b8836b61
Abele Mălan <6689720+AbeleMM@users.noreply.github.com>
2022-10-24 21:02:30 +0200

    Add fine_tuning scripts and README (#2)
    * Set up scrapy & add rosetta code scraper
    
    * Add instance of scraped Rosetta Code dataset
    
    * Add transformers dependency
    
    * Add torch dependency
    
    * Add requirements alternative to poetry
    
    * Add solutions for summing all GCDs of unique number pairs up to N
    
    Source: https://andrewshitov.com/2020/12/07/a-problem-solved-in-22-programming-languages/
    
    * Add sample programs scraper
    
    * Add instance of scraped Sample Programs dataset
    
    * Add solutions from codingbat
    
    Source: https://codingbat.com (removed any comments from code)
    Also, trimmed trailing whitespace from andrewshitov data.
    
    * Add walkccc leetcode scraper
    
    * Add instance of scraped walkccc LeetCode dataset
    
    * Streamline scrapers
    
    * Add openai dependency
    
    * Adding fine_tuning
    
    * Adding output directory to gitignore
    
    * Make script dependent on input
    
    * Making model.py compatible with plbart
    
    * Fixing bug in model.py
    
    * Adding processing scripts
    
    * Adding initial test/val data
    
    * Removing type hints from python examples
    
    * Improving processing script
    
    * Add all scraped data into single file
    
    * Adding properly converted files
    
    * Add intermediate checkpointing
    
    * Making script runnable + splitting data into train and test
    
    * Fixing bug in script to run
    
    * Removing broken fairseq requirement
    
    * Fixing mistake in script with wrong file
    
    * Adjusting output names in script
    
    * Adding converted synthetic data + adding augmented training set
    
    * Cleaning up files + adding initial Readme
    
    * Fixing small mistake in Readme
    
    * Removing unncessary lines from Readme
    
    * Remove unncessary header from Readme
    
    * Add scraping and generation subsections to README
    
    Co-authored-by: AbeleMM <AbeleMM@users.noreply.github.com>
    Co-authored-by: imarijnissen <i.c.w.m.marijnissen@student.tudelft.nl>
    Co-authored-by: ImpMA <50290518+ImpMA@users.noreply.github.com>

---
+++ fine_tuning/train_and_test.sh
@@ -0,0 +1,42 @@
+# shellcheck disable=SC1066
+# shellcheck disable=SC2121
+
+if [ "$1" = "plbart" ]; then
+  pretrained_model="uclanlp/plbart-base"
+  model_type="plbart"
+  config_and_token_name="uclanlp/plbart-base"
+else
+  pretrained_model="microsoft/codebert-base"
+  model_type="roberta"
+  config_and_token_name="roberta-base"
+fi
+
+output_dir="./out"
+python3 run.py \
+	--do_train \
+	--model_type $model_type \
+	--model_name_or_path $pretrained_model \
+	--config_name $config_and_token_name \
+	--tokenizer_name $config_and_token_name \
+	--train_filename ../train.java,../train.py \
+	--output_dir $output_dir \
+	--max_source_length 512 \
+	--max_target_length 512 \
+	--beam_size 5 \
+	--train_batch_size 4 \
+	--learning_rate 0.00005 \
+	--train_steps 50000 \
+
+python3 run.py \
+  --do_test \
+	--model_type $model_type \
+	--model_name_or_path $pretrained_model \
+	--config_name $config_and_token_name \
+	--tokenizer_name $config_and_token_name  \
+	--load_model_path $output_dir/checkpoint-best-ppl/pytorch_model.bin \
+	--dev_filename ../test.java,../test.py \
+	--output_dir $output_dir \
+	--max_source_length 512 \
+	--max_target_length 512 \
+	--beam_size 5 \
+	--eval_batch_size 16
